3a922ffb533d2837fc47c251fb32d7c5def28d02
zard <junyao1152@163.com>
2025-07-22 16:15:46 +0800

    update

--- apk_finder/web_client/src/components/SettingsDialog.vue
+++ apk_finder/web_client/src/components/SettingsDialog.vue
@@ -43,22 +43,7 @@
                   class="input-field pr-20"
                   placeholder="cs"
                 />
-                <div class="absolute inset-y-0 right-0 flex items-center pr-3 space-x-2">
-                  <button
-                    type="button"
-                    @click="toggleTokenVisibility"
-                    class="text-gray-400 hover:text-gray-600 transition-colors"
-                    :title="showToken ? 'Hide token' : 'Show token'"
-                  >
-                    <svg v-if="showToken" class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.878 9.878L8.464 8.464a5 5 0 015.657 5.657L9.878 9.878zM12 3c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21l-6-6m0 0l-6-6"></path>
-                    </svg>
-                    <svg v-else class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                    </svg>
-                  </button>
-                </div>
+
               </div>
               <p class="text-xs text-gray-500 mt-1">
                 Token is encrypted in storage ({{ tokenDisplayText }})
